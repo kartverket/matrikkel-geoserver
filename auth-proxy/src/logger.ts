83a6ddbcfce12ae75724cdf7e19d333944bd91e4
Nicklas Utgaard <nutgaard@gmail.com>
2026-08-07 12:35:54 +0200

    nojira: fix linting

--- auth-proxy/src/logger.ts
+++ auth-proxy/src/logger.ts
@@ -3,7 +3,7 @@ import pino from "pino";
 export const logger = pino({
   level: process.env.LOG_LEVEL ?? "info",
   base: { service: "matrikkel-auth-proxy" },
-  messageKey: 'message',
+  messageKey: "message",
   timestamp: pino.stdTimeFunctions.isoTime,
   formatters: {
     level: (level) => ({ level: level.toUpperCase() }),
